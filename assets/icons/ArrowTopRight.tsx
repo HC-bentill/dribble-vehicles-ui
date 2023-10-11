792339bf2f0ab15a7a3df6f2cdb3964489095570
Henry <hbentil@acsint.io>
2023-10-11 21:53:04 +0000

    home page

---
+++ assets/icons/ArrowTopRight.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const index = () => {
+  return (
+<svg width="20px" height="20px" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M9 8.5H16.5V16" stroke="#121923" stroke-width="1.2"/>
+<path d="M16.5 8.5L7 18" stroke="#121923" stroke-width="1.2"/>
+</svg>
+  );
+};
+
+export default index;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import VanImage from "../assets/img/van-removebg-preview.png";
+import ArrowTopRight from "../assets/icons/ArrowTopRight";
 
 const Home = () => {
   return (
@@ -6,7 +7,9 @@ const Home = () => {
       <div className="van-details">
         <div className="details-container">
           <div className="details-text">
-            <p className="details-value" style={{marginBottom:"20px"}}>Volkswagen Transporter</p>
+            <p className="details-value" style={{marginBottom: "20px"}}>
+              Volkswagen Transporter
+            </p>
 
             <div className="details-flex">
               <div>
@@ -21,27 +24,45 @@ const Home = () => {
 
             <div className="details-flex">
               <div>
-                <p className="details-title">Payload</p>
-                <p className="details-value">2,885 lbs</p>
+                <p className="details-title">Load Length</p>
+                <p className="details-value">117 in</p>
               </div>
               <div>
-                <p className="details-title">Load Volume</p>
-                <p className="details-value">353,937 in</p>
+                <p className="details-title">Load Width</p>
+                <p className="details-value">67 in</p>
               </div>
             </div>
 
-            <div className="details-flex">
+            <div className="details-flex" style={{paddingTop: "5px"}}>
               <div>
-                <p className="details-title">Payload</p>
-                <p className="details-value">2,885 lbs</p>
+                <p className="details-value plate">6TRJ244</p>
               </div>
               <div>
-                <p className="details-title">Load Volume</p>
-                <p className="details-value">353,937 in</p>
+                <p className="underline-dotted">Documents</p>
               </div>
             </div>
           </div>
           <div className="van-image" style={{backgroundImage: `url(${VanImage.src})`, backgroundRepeat: "no-repeat", backgroundPosition: "center center"}}></div>
+        </div>
+      </div>
+
+      <div className="home-body">
+        <div className="w-100  home-body-container">
+          <div className="driver-stats-header">
+            <p className="title">Routes</p>
+            <p className="underline-dotted">History</p>
+          </div>
+        </div>
+
+        <div className="home-body-vertical-container"></div>
+
+        <div className="w-100 home-body-container">
+          <div className="driver-stats-header">
+            <p className="title">Driver Statistics</p>
+            <p className="">
+              <ArrowTopRight />
+            </p>
+          </div>
         </div>
       </div>
     </>
